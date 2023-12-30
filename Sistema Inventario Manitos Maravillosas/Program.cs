3bed08d29d77953ba40fd7160305e50e424de700
TheCsarbeat <ytcesarjs@gmail.com>
2023-12-29 23:02:48 -0600

    Login started
    Basically started to implement the login, didn't really touched the Inventory views at all.

--- Sistema Inventario Manitos Maravillosas/Program.cs
+++ Sistema Inventario Manitos Maravillosas/Program.cs
@@ -1,17 +1,34 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Sistema_Inventario_Manitos_Maravillosas.Areas.Identity.Data;
 using Sistema_Inventario_Manitos_Maravillosas.Data;
+using SistemaInventario.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddRazorPages();
+
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(
     builder.Configuration.GetConnectionString("ConnectionToDataBase")));
+
+builder.Services.AddDbContext<InventarioDbContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("ConnectionToDataBase")));
+
+builder.Services.AddDefaultIdentity<AppUser>(options => options.SignIn.RequireConfirmedAccount = false)
+    .AddEntityFrameworkStores<InventarioDbContext>();
 
 builder.Services.AddHttpClient(); // Register HttpClient
 
 builder.Services.AddSession(); // Add session services
 builder.Services.AddMemoryCache(); // Add memory cache services
+
+builder.Services.Configure<SignInOptions>(options =>
+{
+    options.RequireConfirmedEmail = false;
+});
 
 
 var app = builder.Build();
@@ -28,11 +45,14 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication(); ;
 
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Facturation}/{action=Index}/{id?}");
+
+app.MapRazorPages();
 
 app.Run();
