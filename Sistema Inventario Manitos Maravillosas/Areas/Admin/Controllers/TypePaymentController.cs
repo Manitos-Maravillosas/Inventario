dcf328c68697dc5d02090c9ba6f2bd1f684b4177
Fernanda Murillo <murillomf2001@gmail.com>
2024-01-09 00:23:01 -0600

    TypePayment Half Done
    Consult and Delete ready

---
+++ Sistema Inventario Manitos Maravillosas/Areas/Admin/Controllers/TypePaymentController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Sistema_Inventario_Manitos_Maravillosas.Data.Services;
+using Sistema_Inventario_Manitos_Maravillosas.Models;
+
+namespace Sistema_Inventario_Manitos_Maravillosas.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class TypePaymentController : Controller
+    {
+        private readonly ITypePaymentService _TypePaymentService;
+        public TypePaymentController(ITypePaymentService typePaymentService)
+        {
+            _TypePaymentService = typePaymentService;
+        }
+
+        // GET: TypePaymentController
+        public ActionResult Index()
+        {
+            var typePayments = _TypePaymentService.GetAll();
+
+            return View(typePayments);
+        }
+
+        // GET: TypePaymentController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: TypePaymentController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TypePaymentController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: TypePaymentController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: TypePaymentController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: TypePaymentController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: TypePaymentController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(string id)
+        {
+            OperationResult result = _TypePaymentService.Delete(id);
+            if (!result.Success)
+            {
+                ViewData["ErrorMessage"] = result.Message;
+            }
+            else
+            {
+                ViewData["Success"] = "Se ha eliminado al tipo de pago!";
+            }
+            var clients = _TypePaymentService.GetAll();
+            return View("Index", clients);
+        }
+    }
+}
---
+++ Sistema Inventario Manitos Maravillosas/Data/Services/TypePaymentService.cs
@@ -0,0 +1,139 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using Sistema_Inventario_Manitos_Maravillosas.Areas.Admin.Models;
+using Sistema_Inventario_Manitos_Maravillosas.Models;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Sistema_Inventario_Manitos_Maravillosas.Data.Services
+{
+    public interface ITypePaymentService
+    {
+        List<TypePayment> GetAll();
+        OperationResult Delete(string id);
+
+    }
+
+    public class TypePaymentService : ITypePaymentService
+    {
+        private readonly IConfiguration _configuration;
+
+        private OperationResult result = new OperationResult(true, "");
+
+        public TypePaymentService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        
+        //------------------------------------------------------------------------------------
+        //                              GetAll                                             
+        //------------------------------------------------------------------------------------
+        public List<TypePayment> GetAll()
+        {
+            List<TypePayment> typePayments = new List<TypePayment>();
+            string connectionString = _configuration.GetConnectionString("ConnectionToDataBase");
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    using (SqlCommand command = new SqlCommand("spTypePaymentCRUD", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+
+                        var parameters = new SqlParameter[]
+                        {
+                            new SqlParameter("@idTypePayment", DBNull.Value),
+                            new SqlParameter("@name", DBNull.Value),
+                            new SqlParameter("@idCoin", DBNull.Value),
+                            new SqlParameter("@coinName", DBNull.Value),
+                            new SqlParameter("@operation", '2') 
+                        };
+
+                        command.Parameters.AddRange(parameters);
+
+                        connection.Open();
+
+                        using (SqlDataReader dataReader = command.ExecuteReader())
+                        {
+                            
+                            while (dataReader.Read())
+                            {
+                                TypePayment typePayment = new TypePayment
+                                {
+                                    Id = Convert.ToInt32(dataReader["idTypePayment"]),
+                                    Name = dataReader["name"].ToString(),
+                                    CoinName = dataReader["coinName"].ToString(),
+                                    CoinDescription = dataReader["coinDescription"].ToString()                                    
+                                };
+                                typePayments.Add(typePayment);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                typePayments.Clear();
+                throw new CustomDataException(ex.Message, ex);
+            }
+
+            return typePayments;
+        }
+
+        //------------------------------------------------------------------------------------
+        //                              Delete                                             
+        //------------------------------------------------------------------------------------
+        public OperationResult Delete(string id)
+        {
+            string connectionString = _configuration.GetConnectionString("ConnectionToDataBase");
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    using (SqlCommand command = new SqlCommand("spTypePaymentCRUD", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+
+                        var parameters = new SqlParameter[]
+                        {
+                            new SqlParameter("@idTypePayment", id),
+                            new SqlParameter("@name", DBNull.Value),
+                            new SqlParameter("@idCoin", DBNull.Value),
+                            new SqlParameter("@coinName", DBNull.Value),
+                            new SqlParameter("@operation", '4')
+                        };
+
+                        command.Parameters.AddRange(parameters);
+
+                        connection.Open();
+                        command.ExecuteReader();
+                        connection.Close();
+                    }
+                }
+            }
+            catch (SqlException sqlEx)
+            {
+                if (sqlEx.Number == 50000)
+                {
+                    result.Success = false;
+                    result.Message = sqlEx.Message;
+                    return result;
+                }
+                else
+                {
+                    throw new CustomDataException("Error executing SQL command: " + sqlEx.Message, sqlEx);
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                throw new CustomDataException("An error occurred: " + ex.Message, ex);
+            }
+
+            return result;
+        }
+
+    }
+}
