6ead06d51ec3f12dbcb2fab2c43981a584cf458b
Chacalerks <erksmartinez2014@gmail.com>
2024-01-16 00:24:38 -0600

    div Money exchange css

--- Sistema Inventario Manitos Maravillosas/wwwroot/js/site.js
+++ Sistema Inventario Manitos Maravillosas/wwwroot/js/site.js
@@ -1,1 +1,9 @@
-﻿
+﻿// script.js
+document.getElementById("botonDesplegable").addEventListener("click", function () {
+    var opciones = document.getElementById("opcionesMoneda");
+    if (opciones.style.display === "block") {
+        opciones.style.display = "none";
+    } else {
+        opciones.style.display = "block";
+    }
+});
