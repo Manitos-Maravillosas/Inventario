30570460e3a2cbee9974536d9dafb7dbef772d55
Fernanda Murillo <murillomf2001@gmail.com>
2024-01-15 21:16:28 -0600

    Delete and read provider
    Delete and read provider

---
+++ Sistema Inventario Manitos Maravillosas/Areas/Supplier/Controllers/ProviderController.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Sistema_Inventario_Manitos_Maravillosas.Areas.Supplier.Models;
+using Sistema_Inventario_Manitos_Maravillosas.Data.Services;
+using Sistema_Inventario_Manitos_Maravillosas.Models;
+
+namespace Sistema_Inventario_Manitos_Maravillosas.Areas.Supplier.Controllers
+{
+    [Area("Supplier")]
+    public class ProviderController : Controller
+    {
+        private readonly IProviderService _ProviderService;
+        public ProviderController(IProviderService providerService)
+        {
+            _ProviderService = providerService;
+        }
+        // GET: ProviderController
+        public ActionResult Index()
+        {
+            var providers = _ProviderService.GetAll();
+
+            return View(providers);
+        }
+
+        // GET: ProviderController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ProviderController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProviderController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Provider provider)
+        {
+            if (ModelState.IsValid)
+            {
+                OperationResult result = _ProviderService.Add(provider);
+                if (!result.Success)
+                {
+                    ViewData["ErrorMessage"] = result.Message;
+                }
+                ViewData["Success"] = "Proveedor agregado correctamente!";
+
+            }
+
+            return View(provider);
+        }
+
+        // GET: ProviderController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ProviderController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProviderController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ProviderController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(string id)
+        {
+            OperationResult result = _ProviderService.Delete(id);
+            if (!result.Success)
+            {
+                ViewData["ErrorMessage"] = result.Message;
+            }
+            else
+            {
+                ViewData["Success"] = "Se ha eliminado al proveedor!";
+            }
+            var providers = _ProviderService.GetAll();
+            return View("Index", providers);
+        }
+    }
+}
---
+++ Sistema Inventario Manitos Maravillosas/Data/Services/ProviderService.cs
@@ -0,0 +1,186 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using Sistema_Inventario_Manitos_Maravillosas.Areas.Supplier.Models;
+using Sistema_Inventario_Manitos_Maravillosas.Models;
+using System.Data;
+using System.Data.SqlClient;
+using System.Net;
+
+namespace Sistema_Inventario_Manitos_Maravillosas.Data.Services
+{
+    public interface IProviderService
+    {
+        List<Provider> GetAll();
+        OperationResult Add(Provider newProvider);
+        OperationResult Delete(string id);
+
+    }
+
+    public class ProviderService : IProviderService
+    {
+        private readonly IConfiguration _configuration;
+
+        private OperationResult result = new OperationResult(true, "");
+
+        public ProviderService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        //------------------------------------------------------------------------------------
+        //                              GetAll                                             
+        //------------------------------------------------------------------------------------
+        public List<Provider> GetAll()
+        {
+            List<Provider> providers = new List<Provider>();
+            string connectionString = _configuration.GetConnectionString("ConnectionToDataBase");
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    using (SqlCommand command = new SqlCommand("spProviderCRUD", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+
+                        var parameters = new SqlParameter[]
+                        {
+                            new SqlParameter("@idProvider", DBNull.Value),
+                            new SqlParameter("@name", DBNull.Value),
+                            new SqlParameter("@phoneNumber", DBNull.Value),
+                            new SqlParameter("@description", DBNull.Value),
+                            new SqlParameter("@operation", '2')
+                        };
+
+                        command.Parameters.AddRange(parameters);
+
+                        connection.Open();
+
+                        using (SqlDataReader dataReader = command.ExecuteReader())
+                        {
+                            while (dataReader.Read())
+                            {
+                                Provider provider = new Provider
+                                {
+                                    Id = Convert.ToInt32(dataReader["idProvider"]),
+                                    Name = dataReader["name"].ToString(),
+                                    PhoneNumber = dataReader["phoneNumber"].ToString(),
+                                    Description = dataReader["description"].ToString(),
+                                };
+                                providers.Add(provider);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                providers.Clear();
+                throw new CustomDataException(ex.Message, ex);
+            }
+
+            return providers;
+        }
+
+        //------------------------------------------------------------------------------------
+        //                              Add                                             
+        //------------------------------------------------------------------------------------
+        public OperationResult Add(Provider newProvider)
+        {
+            string connectionString = _configuration.GetConnectionString("ConnectionToDataBase");
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    using (SqlCommand command = new SqlCommand("spProviderCRUD", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+
+                        command.Parameters.Add(new SqlParameter("@idProvider", newProvider.Id != 0 ? (object)newProvider.Id : DBNull.Value));
+                        command.Parameters.Add(new SqlParameter("@name", string.IsNullOrEmpty(newProvider.Name) ? DBNull.Value : newProvider.Name));
+                        command.Parameters.Add(new SqlParameter("@phoneNumber", string.IsNullOrEmpty(newProvider.PhoneNumber) ? DBNull.Value : newProvider.PhoneNumber));
+                        command.Parameters.Add(new SqlParameter("@description", string.IsNullOrEmpty(newProvider.Description) ? DBNull.Value : newProvider.Description));
+                        command.Parameters.Add(new SqlParameter("@operation", 1));
+
+                        connection.Open();
+                        command.ExecuteNonQuery();
+                        connection.Close();
+                    }
+                }
+            }
+            catch (SqlException sqlEx)
+            {
+                if (sqlEx.Number == 50000)
+                {
+                    result.Success = false;
+                    result.Message = sqlEx.Message;
+                    return result;
+                }
+                else
+                {
+                    throw new CustomDataException("Error executing SQL command: " + sqlEx.Message, sqlEx);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new CustomDataException("An error occurred: " + ex.Message, ex);
+            }
+
+            return result;
+        }
+
+        //------------------------------------------------------------------------------------
+        //                              Delete                                             
+        //------------------------------------------------------------------------------------
+        public OperationResult Delete(string id)
+        {
+            string connectionString = _configuration.GetConnectionString("ConnectionToDataBase");
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    using (SqlCommand command = new SqlCommand("spProviderCRUD", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+
+                        var parameters = new SqlParameter[]
+                        {
+                            new SqlParameter("@idProvider", id),
+                            new SqlParameter("@name", DBNull.Value),
+                            new SqlParameter("@phoneNumber", DBNull.Value),
+                            new SqlParameter("@description", DBNull.Value),
+                            new SqlParameter("@operation", '4')
+                        };
+
+                        command.Parameters.AddRange(parameters);
+
+                        connection.Open();
+                        command.ExecuteReader();
+                        connection.Close();
+                    }
+                }
+            }
+            catch (SqlException sqlEx)
+            {
+                if (sqlEx.Number == 50000)
+                {
+                    result.Success = false;
+                    result.Message = sqlEx.Message;
+                    return result;
+                }
+                else
+                {
+                    throw new CustomDataException("Error executing SQL command: " + sqlEx.Message, sqlEx);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new CustomDataException("An error occurred: " + ex.Message, ex);
+            }
+            return result;
+        }
+               
+        
+    }
+}
